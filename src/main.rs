44568518a7c8ce95e75e8a380b1aab5fa323826f
Tokigin <134780921+Tokigin@users.noreply.github.com>
2024-11-06 18:02:07 +0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-fn main(){
-    let x:i32 = 50;
-    let y:u32 = 100;
-    println!("X - {x} , Y - {y}");
-
+fn main() {
+    let x: i32 = 50;
+    let y: i32 = 100;
+    let z: i32 = x + y;
+    let numbers: [i32; 6] = [1, 2, 3, 4, 5, 6];
+    println!("X - {x} , Y - {y}, Z - {z}");
+    println!("Number - {:?}", numbers);
 }

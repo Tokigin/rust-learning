f530cc4cb154bad94cc8658e1eaf7a89ab74b155
Tokigin <134780921+Tokigin@users.noreply.github.com>
2024-11-07 15:54:31 +0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+use rand::Rng;
 fn merge_sort(arr: &mut Vec<i32>) {
-    let len = arr.len();
+    let len: usize = arr.len();
     if len < 2 {
         return; // Base case: if the array has less than 2 elements, it's already sorted.
     }
 
-    let mid = len / 2;
-    let mut left = arr[0..mid].to_vec(); // Create a left subarray
-    let mut right = arr[mid..len].to_vec(); // Create a right subarray
+    let mid: usize = len / 2;
+    let mut left: Vec<i32> = arr[0..mid].to_vec(); // Create a left subarray
+    let mut right: Vec<i32> = arr[mid..len].to_vec(); // Create a right subarray
 
     merge_sort(&mut left); // Sort the left subarray
     merge_sort(&mut right); // Sort the right subarray
@@ -42,9 +43,36 @@ fn merge(arr: &mut Vec<i32>, left: &[i32], right: &[i32]) {
         k += 1;
     }
 }
+fn search(arr: &mut Vec<i32>) {
+    let low_bound: i32 = 5;
+    let up_bound: i32 = 8;
 
+    // Find lower bound index using binary search
+    let low_index: usize = arr.binary_search(&low_bound).unwrap_or_else(|x: usize| x);
+
+    // Find upper bound index using binary search
+    let up_index: usize = arr
+        .binary_search(&(up_bound + 1))
+        .unwrap_or_else(|x: usize| x);
+
+    // Count how many numbers fall between 1000 and 2000
+    let count: usize = up_index - low_index;
+
+    // Print the count of numbers in the specified range
+    println!("Count of numbers between {low_bound} and {up_bound}: {count}");
+}
 fn main() {
-    let mut arr: Vec<i32> = vec![64, 34, 25, 12, 22, 11, 90];
-    merge_sort(&mut arr);
-    println!("Sorted array: {:?}", arr);
+    let mut rng = rand::thread_rng();
+    let mut numbers: Vec<i32> = (0..10000).map(|_| rng.gen_range(1..=10000)).collect();
+    println!("Random Numbers: {:?}", numbers);
+    merge_sort(&mut numbers);
+    println!("Sorted array: {:?}", numbers);
+    search(&mut numbers);
+    let numbers: Vec<i32> = vec![3, 4, 4, 4, 5];
+    let num = 3;
+    let result: Result<usize, usize> = numbers.binary_search(&num);
+    match result {
+        Ok(index) => println!("{}", index), // Print only the index if found
+        Err(_) => println!("Not found"),    // Handle not found case
+    }
 }
